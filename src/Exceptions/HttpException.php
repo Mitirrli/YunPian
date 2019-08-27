8b84dc72540c4a7ac281f9b434a8c24527160858
黄磊 <huangl@golive.cc>
2019-08-27 19:03:39 +0800

    集成CI自动修复代码格式

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the mitirrli/YunPian
+ *
+ * (c) mitirrli <512663808@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Mitirrli\YunPian\Exceptions;
 
 
--- src/YunPian.php
+++ src/YunPian.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the mitirrli/YunPian
+ *
+ * (c) mitirrli <512663808@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Mitirrli\YunPian;
 
 
@@ -21,22 +30,37 @@ class YunPian
         $this->tpl_id = $tpl_id;
     }
 
+    /**
+     * @return Client
+     */
     public function getHttpClient()
     {
         return new Client($this->guzzleOptions);
     }
 
+    /**
+     * @param array $options
+     */
     public function setGuzzleOptions(array $options)
     {
         $this->guzzleOptions = $options;
     }
 
-    public function getCode($code, $mobile, $format = 'json')
+    /**
+     * 发送短信验证码
+     *
+     * @param $code int 验证码
+     * @param $mobile int 手机号
+     * @return mixed|string
+     * @throws HttpException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function sendCode($code, $mobile)
     {
         $url = 'https://sms.yunpian.com/v1/sms/tpl_send.json';
 
         $query = [
-            'tpl_id'    => $this->tpl_id,
+            'tpl_id' => $this->tpl_id,
             'tpl_value' => urlencode('#code#') . '=' . urlencode($code), 'apikey' => $this->key, 'mobile' => $mobile,
         ];
 
@@ -44,7 +68,8 @@ class YunPian
             $response = $this->getHttpClient()
                 ->request('POST', $url, ['form_params' => $query])
                 ->getBody()->getContents();
-            return 'json' === $format ? \json_decode($response, true) : $response;
+
+            return \json_decode($response, true);
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode());
         }
