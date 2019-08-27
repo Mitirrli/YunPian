63750b1c6ca73da26ee29ad273a7a7ccdfb03946
Mitirrli <709758582@qq.com>
2019-08-27 10:53:55 +0000

    Apply fixes from StyleCI

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,9 +1,16 @@
 <?php
 
-namespace Mitirrli\YunPian\Exceptions;
+/*
+ * This file is part of the mitirrli/YunPian
+ *
+ * (c) mitirrli <512663808@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
+namespace Mitirrli\YunPian\Exceptions;
 
 class HttpException extends \Exception
 {
-
 }
