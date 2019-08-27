8b509490eda01829fe48673a8ad9f0b408d1a16e
黄磊 <huangl@golive.cc>
2019-08-27 18:28:17 +0800

    修改guzzle请求为表单请求

--- src/YunPian.php
+++ src/YunPian.php
@@ -4,7 +4,6 @@ namespace Mitirrli\YunPian;
 
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
 use Mitirrli\YunPian\Exceptions\HttpException;
 
 class YunPian
@@ -15,8 +14,6 @@ class YunPian
 
     /**
      * YunPian constructor.
-     * @param $key
-     * @param $tpl_id
      */
     public function __construct($key, $tpl_id)
     {
@@ -24,30 +21,16 @@ class YunPian
         $this->tpl_id = $tpl_id;
     }
 
-    /**
-     * @return Client
-     */
     public function getHttpClient()
     {
         return new Client($this->guzzleOptions);
     }
 
-    /**
-     * @param array $options
-     */
     public function setGuzzleOptions(array $options)
     {
         $this->guzzleOptions = $options;
     }
 
-    /**
-     * @param $code
-     * @param $mobile
-     * @param string $format
-     * @return mixed|string
-     * @throws GuzzleException
-     * @throws HttpException
-     */
     public function getCode($code, $mobile, $format = 'json')
     {
         $url = 'https://sms.yunpian.com/v1/sms/tpl_send.json';
@@ -59,7 +42,7 @@ class YunPian
 
         try {
             $response = $this->getHttpClient()
-                ->request('POST', $url, ['json' => $query])
+                ->request('POST', $url, ['form_params' => $query])
                 ->getBody()->getContents();
             return 'json' === $format ? \json_decode($response, true) : $response;
         } catch (\Exception $e) {
